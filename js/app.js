c0405e00432da4a6f56ec7ae5d8e22712e98c86c
Josh Doyle <me@joshdoyle.us>
2019-12-19 16:08:56 -0600

    Animated Tamagotchi div to move around screen

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ const game = {
 	startTime(){
 		this.timer = setInterval(() => {
 			this.time++
+			animateDiv()
 			},1000)
 
 	},
@@ -38,7 +39,52 @@ const game = {
 
 
 
+/////animation
 
+// give credit for this code
+//http://jsfiddle.net/Xw29r/15/
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    var oldq = $('#tama').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $('#tama').animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv();        
+    });
+    
+};
+
+function calcSpeed(prev, next) {
+    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+}
+
+
+/////animation
 
 //Listeners
 $( "form" ).submit((event) => {

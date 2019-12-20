59784a0f30c2da2456aaf9159ef05ea995a03619
Josh Doyle <me@joshdoyle.us>
2019-12-20 11:40:55 -0600

    Functional. No styling. Code needs refactoring

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ class Tamagotchi {
 		this.boredom = 0
 		this.sleepiness = 0
 		this.sleeping = false
+		this.height = 50
+		this.width = 50
+		this.opacity = 1
 	}
 
 	// Called every 'hour' which is every 10 seconds(see game.hourInMilliseconds = 10000)
@@ -17,26 +20,48 @@ class Tamagotchi {
 	}
 
 	getAge(){
-		return Math.floor(this.age)
+		return Math.round( this.age * 10) / 10
 	}
 
 	getSleepiness(){
-		return Math.floor(this.sleepiness)
+		return Math.round( this.sleepiness * 10) / 10
+	}
+
+	getHunger(){
+		console.log('in getHunger', this.hunger)
+		return Math.round( this.hunger * 10) / 10
+	}
+
+	getBoredom(){
+		return Math.round( this.boredom * 10) / 10
 	}
 
 	updateAge(){
 		this.age += 1/24 //Add an hour to age
 	}
 
+	getWidth(){
+		return this.width
+	}
+
+	getHeight(){
+		return this.height
+	}
+
 	updateHunger(){
+	
 		let hungerFactor = 0
 		if(this.sleeping){
-			let hungerFactor = .25
+			hungerFactor = .25
+			this.width -= 5
+			this.height -= 5
 		} else {
-			let hungerFactor = 1
+			hungerFactor = 1
+			this.width -= 10
+			this.height -= 10
 		}
-		this.hunger += hungerFactor //Every hour you are 10% hungrier
 
+		this.hunger += hungerFactor //Every hour you are 10% hungrier
 	}
 
 	updateBoredom(){
@@ -45,6 +70,7 @@ class Tamagotchi {
 			boredomFactor = .1
 		}else {
 			boredomFactor = 2
+			this.opacity -= .2
 		}
 		this.boredom += boredomFactor //Every hour you are 20% more bored
 	}
@@ -54,7 +80,7 @@ class Tamagotchi {
 			this.sleepiness -= 1/8
 		} else {
 			this.sleepiness += 1/2 //You can be awake for 16 hours a day.
-		
+			speed -= 5000
 		}
 
 		if(this.sleepiness < 0){ this.sleepiness = 0}
@@ -63,6 +89,7 @@ class Tamagotchi {
 	isAlive(){
 		if(this.hunger >= 10 || this.boredom >= 10 || this.sleepiness >=  10){
 			console.log('tama is dead')
+			speed = 0
 			return false
 		} else {
 			return true
@@ -70,13 +97,18 @@ class Tamagotchi {
 	}
 
 	feed(){ 
+		console.log('in Tama feed')
 		this.hunger -= 2
 		if(this.hunger < 0){this.hunger = 0}
+		this.width += 5
+		this.height += 5	
 	}
 
 	play(){ 
 		this.boredom -= 1
 		if(this.boredom < 0){this.boredom = 0}
+		this.opacity += .1	
+		console.log('opacity', this.opacity)
 	}
 
 	toggleSleep(lightsOn){
@@ -86,6 +118,7 @@ class Tamagotchi {
 		} else {
 			console.log('is sleeping')
 			this.sleeping = true
+			speed += 1000
 		}
 	}
 	
@@ -101,6 +134,7 @@ const game = {
 	seconds: 0,
 	timer: null,
 	lightsOn: true,
+	// speed: 20000,
 
 
 	play(n){
@@ -121,7 +155,10 @@ const game = {
 		// let timer = setInterval(this.waitForTheHour(),1000)
 		 this.timer = setInterval(() => {
 		 	if(this.myTama.isAlive()){
-				animateDiv()
+		 		console.log('before moving. am i asleep?',this.myTama.sleeping)
+		 		if(this.myTama.sleeping === false){
+					animateDiv()
+		 		}
 				if(this.seconds === this.hourInSeconds){
 					this.passTheHour()
 					this.seconds = 0
@@ -129,6 +166,7 @@ const game = {
 					this.seconds++
 				}
 		 	} else {
+		 		$('#pen').append(`<h1>${this.myTama.name} is dead`)
 		 		this.stopTime()
 		 	}
 		},1000)
@@ -137,10 +175,10 @@ const game = {
 	passTheHour(){
 			this.myTama.updateStats()
 			this.printStats()
+
 	},
 
 	stopTime(){
-		console.log('stop timer')
 		clearInterval(this.timer)
 		this.printStats()
 	},
@@ -148,18 +186,21 @@ const game = {
 	printStats(){
 		$('#name').text('Name: ' + this.myTama.name)
 		$('#age').text('Age: ' + this.myTama.getAge() + ' Days')
-		$('#hunger').text('Hunger: ' + this.myTama.hunger)
-		$('#boredom').text('Boredom: ' + this.myTama.boredom)
+		$('#hunger').text('Hunger: ' + this.myTama.getHunger())
+		$('#boredom').text('Boredom: ' + this.myTama.getBoredom())
 		$('#sleepiness').text('Sleepiness: ' + this.myTama.getSleepiness())
+
+		$('#tama').css('width', this.myTama.getWidth())
+		$('#tama').css('height', this.myTama.getHeight())
+		$('#tama').css('opacity', this.myTama.opacity)
+
 	},
 
 	switchLights(){
 		if(this.lightsOn){
-			console.log('lights turned off')
 			this.lightsOn = false
 			this.myTama.toggleSleep(this.lightsOn)
 		} else {
-			console.log('lights turned on')
 			this.lightsOn = true
 			this.myTama.toggleSleep(this.lightsOn)			
 		}
@@ -175,6 +216,9 @@ const game = {
 // give credit for this code
 //http://jsfiddle.net/Xw29r/15/
 //move these functions to tamagotchi class
+
+let speed = 20000
+
 function makeNewPosition(){
     
     // Get viewport dimensions (remove the dimension of the div)
@@ -190,7 +234,7 @@ function makeNewPosition(){
 
 function animateDiv(myclass){
     var newq = makeNewPosition();
-    $('#tama').animate({ top: newq[0], left: newq[1] }, 30000,   function(){
+    $('#tama').animate({ top: newq[0], left: newq[1] }, speed,   function(){
       animateDiv('#tama');        
     });
     
